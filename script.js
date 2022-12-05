63cc81297baa5bc05b2d2bd1d68ef7f1708875ee
Wixom78666 <jswix4@gmail.com>
2022-12-04 20:16:02 -0600

    made the api log my text when i press enter

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-var apiKey = "892b4b6f3d3c699b11445dbe53813867"
+var apiKey = "892b4b6f3d3c699b11445dbe53813867";
+var citySearch = document.getElementById("user-city-choice");
+var searchButton = document.getElementById("searchbtn");
+function getApi(event) {
+    event.preventDefault();
+    var requestUrl ="https://api.openweathermap.org/data/2.5/weather?q=" + citySearch.value + "&appid=" + apiKey;
+
+    fetch(requestUrl) 
+        .then(function (response) {
+            return response.json();
+
+            })
+            .then(function(data) {
+                console.log(data)
+            
+            });
+        }
+
+
+
+searchButton.addEventListener("click", getApi);
+
