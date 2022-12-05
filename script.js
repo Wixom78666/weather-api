e4ce7fc3c649425de35d1e9a8d99d8b5af1f7f21
Wixom78666 <jswix4@gmail.com>
2022-12-04 22:33:03 -0600

    got content from api to show on screen dynamically

--- script.js
+++ script.js
@@ -12,7 +12,20 @@ function getApi(event) {
             })
             .then(function(data) {
                 console.log(data)
-            
+                var currentTemp = data.main.temp;
+                var currentWind = data.wind.speed;
+                var currentHumidity = data.main.humidity;
+                var cityName = data.name
+                console.log(currentTemp)
+                var tempHtml = document.getElementById("temp");
+                var windHtml = document.getElementById("wind");
+                var humidityHtml = document.getElementById("humidity");
+                var cityTitile = document.getElementById("cityHead");
+                tempHtml.innerHTML = "Current Temperature: " + Math.floor((currentTemp - 273.15) * (9/5) + 32) + " ºF";
+                windHtml.textContent = "Wind Speed: " + currentWind + " MPH";
+                humidityHtml.textContent = "Humidity: " + currentHumidity + " %";
+                cityTitile.innerHTML = cityName
+
             });
         }
 
